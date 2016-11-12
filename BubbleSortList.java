02a31b94f63eb43dd5cbe6f557c0105e2ff4cff4
Jonathan Silal <jsilal@albany.edu>
2016-11-12 13:48:25 -0500

    Creating an array for the list

---
+++ BubbleSortList.java
@@ -0,0 +1,15 @@
+public class BubbleSortList
+{
+// Maximum size of ArrayList = Array Size
+public final int Maximum_Size = 20;
+
+// Array for list
+private int[] arr;
+private int currentLength;
+
+public BubbleSortList()
+{
+arr = new int [Maximum_Size];
+currentLength = 0;
+}
+}
